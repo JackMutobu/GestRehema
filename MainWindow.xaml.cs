58e51bfc137952357533958e15bbef4c853caf1a
JackMutobu <jmutobu@outlook.com>
2021-03-04 10:22:39 +0200

    Initial release

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using GestRehema.Views;
 using ModernWpf;
+using ReactiveUI;
+using System.Reactive.Linq;
 using System.Windows;
+using System;
+using Squirrel;
 
 namespace GestRehema
 {
@@ -16,7 +20,23 @@ namespace GestRehema
 
             
             ThemeButton.Click += ThemeButton_Click;
+            this.Events()
+                .Loaded
+                .ObserveOn(RxApp.MainThreadScheduler)
+                .Subscribe(async _ =>
+                {
+                    try
+                    {
+                        using var updateManager = new UpdateManager(@"E:\Project2021\FabProject\Publish");
+                        await updateManager.UpdateApp();
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                },ex => MessageBox.Show(ex.Message));
         }
+
 
         private void ThemeButton_Click(object sender, RoutedEventArgs e)
         {
